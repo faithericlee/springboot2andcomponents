363903571ba649ad5dc856d0b875e0156efbf1b7
xiaodong.li <xiaodong.li@supwisdom.com>
2020-02-09 11:36:03 +0800

    初始项目

---
+++ src/main/java/com/cs/springboot2andcomponents/Springboot2andcomponentsApplication.java
@@ -0,0 +1,13 @@
+package com.cs.springboot2andcomponents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2andcomponentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2andcomponentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cs/springboot2andcomponents/Springboot2andcomponentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.springboot2andcomponents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2andcomponentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
